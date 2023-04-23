0df3a2c3ea89cb10a4107fd79f2f8fb4954df7c4
patrick-stoeckli <patrick_stoeckli@outlook.com>
2023-04-24 00:39:11 +0200

    new separate sort file

---
+++ code/01_sort_basic_data.R
@@ -0,0 +1,29 @@
+library("swissparl")
+library(tidyverse)
+
+load('data/data_raw/BusinessResponsibility_raw.Rdata')
+load('data/data_raw/SubjectBusiness_raw.Rdata')
+load('data/data_raw/BusinessRole_raw.Rdata')
+load('data/data_raw/MemberParty_raw.Rdata')
+
+BusinessResponsibility <- BusinessResponsibility %>% 
+  filter(DepartmentNumber == 9) %>% 
+  select(BusinessNumber, DepartmentNumber, DepartmentName, DepartmentAbbreviation)
+
+SubjectBusiness <- SubjectBusiness %>% 
+  filter(BusinessNumber %in% BusinessResponsibility$BusinessNumber) %>%
+  select(IdSubject, BusinessNumber, TitleDE)
+
+BusinessRole <- BusinessRole %>%            #MemberCouncilNumber
+  filter(BusinessNumber %in% SubjectBusiness$BusinessNumber) %>%
+  #filter(BusinessTypeName == "Motion")
+  select(BusinessNumber, MemberCouncilNumber, BusinessShortNumber, BusinessTitle, BusinessType, BusinessTypeName)
+
+MemberParty <- MemberParty %>%
+  select(PersonIdCode, FirstName, LastName, PartyNumber, PartyName, PartyAbbreviation)
+
+df1 <- merge(BusinessRole, SubjectBusiness, by = "BusinessNumber", all.x = TRUE)
+df2 <- merge(df1, BusinessResponsibility, by = "BusinessNumber", all.x = TRUE)
+df3 <- merge(df2, MemberParty, by.x = "MemberCouncilNumber", by.y = "PersonIdCode" ,all.x = TRUE)
+
+save(df3, file = 'data/data_raw/Basic_raw.Rdata') 
