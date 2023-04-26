c7d39ce8da979d76efb7a095ee5d427976ebb13b
patrick-stoeckli <patrick_stoeckli@outlook.com>
2023-04-26 22:32:07 +0200

    rename parties

--- code/04_graphs.R
+++ code/04_graphs.R
@@ -37,3 +37,38 @@ print(data_sorted)
 
 ggplot(data_sorted, aes(x = ParteiMotion, y = ParteiVotum, fill = Wert)) +
   geom_tile()
+
+load(file = 'data/data_raw/first_try.Rdata')
+
+Parteien <- c("Grüne","SP","GLP","BD","CVP","FDP","SVP")
+
+first_try <- first_try %>% 
+  mutate(value = as.numeric(value)) %>% 
+  select(ParlGroupName,SpeakerParlGroupName, value) %>% 
+  filter(is.na(ParlGroupName) == FALSE) %>%   
+  mutate(ParlGroupName = ifelse(ParlGroupName == "Die Mitte-Fraktion. Die Mitte. EVP.", "CVP", 
+                          ifelse(ParlGroupName == "Grünliberale Fraktion", "GLP", 
+                            ifelse(ParlGroupName == "Grüne Fraktion", "Grüne",      
+                              ifelse(ParlGroupName == "Fraktion der Schweizerischen Volkspartei", "SVP",
+                                ifelse(ParlGroupName == "FDP-Liberale Fraktion", "FDP",
+                                  ifelse(ParlGroupName == "Sozialdemokratische Fraktion", "SP",
+                                    ifelse(ParlGroupName == "Fraktion BD", "BD",
+                                      ParlGroupName)))))))
+         ) %>% 
+  mutate(SpeakerParlGroupName = ifelse(SpeakerParlGroupName == "CVP-Fraktion", "CVP", 
+                                  ifelse(SpeakerParlGroupName == "Grünliberale Fraktion", "GLP", 
+                                    ifelse(SpeakerParlGroupName == "Grüne Fraktion", "Grüne",      
+                                      ifelse(SpeakerParlGroupName == "Fraktion der Schweizerischen Volkspartei", "SVP",
+                                        ifelse(SpeakerParlGroupName == "FDP-Liberale Fraktion", "FDP",
+                                          ifelse(SpeakerParlGroupName == "Sozialdemokratische Fraktion", "SP",
+                                            ifelse(SpeakerParlGroupName == "Fraktion BD", "BD",
+                                              SpeakerParlGroupName)))))))
+         ) %>%
+  mutate(ParlGroupName = factor(ParlGroupName, levels = Parteien),
+         SpeakerParlGroupName = factor(SpeakerParlGroupName, levels = Parteien)) %>%
+  arrange(ParlGroupName, SpeakerParlGroupName)
+
+ggplot(first_try, aes(x = ParlGroupName, y = SpeakerParlGroupName, fill = value)) +
+  geom_tile() +
+  scale_fill_gradient2(low = "red", mid = "white", high = "green", midpoint = 0, limits = c(-1, 1))
+
