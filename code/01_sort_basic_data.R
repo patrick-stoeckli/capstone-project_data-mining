f152fa6bfafcdcf44f8b6ce27f3f50713eb27067
patrick-stoeckli <patrick_stoeckli@outlook.com>
2023-04-26 22:33:47 +0200

    add neccesary data such that files can be compiled

--- code/01_sort_basic_data.R
+++ code/01_sort_basic_data.R
@@ -1,14 +1,17 @@
-library("swissparl")
 library(tidyverse)
 
 load('data/data_raw/BusinessResponsibility_raw.Rdata')
 load('data/data_raw/SubjectBusiness_raw.Rdata')
-load('data/data_raw/BusinessRole_raw.Rdata')
+load('data/data_raw/BusinessRole_raw.Rdata') 
+load('data/data_raw/ParlGroup_raw.Rdata')
 load('data/data_raw/MemberParty_raw.Rdata')
+load('data/data_raw//MemberParlGroup_raw.Rdata')
+load('data/data_raw//Rapporteur_raw.Rdata')
+load('data/data_raw//MemberCouncil_raw.Rdata')
 
 BusinessResponsibility <- BusinessResponsibility %>% 
   filter(DepartmentNumber == 9) %>% 
-  select(BusinessNumber, DepartmentNumber, DepartmentName, DepartmentAbbreviation)
+  select(BusinessNumber)
 
 SubjectBusiness <- SubjectBusiness %>% 
   filter(BusinessNumber %in% BusinessResponsibility$BusinessNumber) %>%
@@ -22,14 +25,29 @@ SubjectBusiness <- SubjectBusiness %>%
 BusinessRole <- BusinessRole %>%            #MemberCouncilNumber
   filter(BusinessNumber %in% SubjectBusiness$BusinessNumber) %>%
   filter(RoleName == "Urheber(-in)") %>%
+  filter(is.na(ParlGroupNumber) == TRUE) %>%
+  filter(is.na(CantonNumber) == TRUE) %>%
+  filter(is.na(CommitteeNumber) == TRUE) %>%
   #filter(BusinessTypeName == "Motion")
-  select(BusinessNumber, MemberCouncilNumber, ParlGroupNumber, CantonNumber, CommitteeNumber, BusinessShortNumber, BusinessTitle, BusinessType, BusinessTypeName)
-
-MemberParty <- MemberParty %>%
-  select(PersonIdCode, FirstName, LastName, PartyNumber, PartyName, PartyAbbreviation)
+  select(BusinessNumber, BusinessShortNumber, BusinessTitle, BusinessTypeName, MemberCouncilNumber, ParlGroupNumber)
 
 df1 <- merge(BusinessRole, SubjectBusiness, by = "BusinessNumber", all.x = TRUE)
-df2 <- merge(df1, BusinessResponsibility, by = "BusinessNumber", all.x = TRUE)
-df3 <- merge(df2, MemberParty, by.x = "MemberCouncilNumber", by.y = "PersonIdCode" ,all.x = TRUE)
 
-save(df3, file = 'data/data_raw/Basic_raw.Rdata') 
+# MemberParty <- MemberParty %>%
+#   select(PersonIdCode, FirstName, LastName, PartyNumber, PartyName, PartyAbbreviation)
+
+# MemberParlGroup <- MemberParlGroup %>%
+#   select(PersonIdCode, FirstName, LastName, ParlGroupNumber, ParlGroupName)
+
+MemberCouncil <- MemberCouncil %>%
+  select(PersonNumber, PersonIdCode, FirstName, LastName, PersonIdCode, ParlGroupName)
+
+Rapporteur <- Rapporteur %>%
+  select(FirstName, LastName, MemberCouncilNumber) %>%
+  unique()
+
+df2 <- merge(Rapporteur, MemberCouncil, by = c("FirstName", "LastName"), all.x = TRUE)
+
+Basic_raw <- merge(df1, df2, by = "MemberCouncilNumber", all.x = TRUE)
+
+save(Basic_raw, file = 'data/data_raw/Basic_raw.Rdata') 
